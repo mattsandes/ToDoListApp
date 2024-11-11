72ecef8491cb9b8636aa27c910c762b0838734f6
Mateus Sandes <mateussandes@hotmail.com.br>
2024-11-11 20:05:27 -0300

    Adicionda a classe que vai receber as validações

--- toDoList/src/main/java/br/com/sandes/controllers/TaskController.java
+++ toDoList/src/main/java/br/com/sandes/controllers/TaskController.java
@@ -1,24 +1,27 @@
 package br.com.sandes.controllers;
 
 import br.com.sandes.model.Task;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.sandes.services.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.UUID;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
 
+    @Autowired
+    private TaskService service;
+
     @GetMapping
-    public Task findAll(){
-        return new Task(
-                UUID.randomUUID(),
-                "Estudar java ate 13:40",
-                "Estudar os assuntos de generics e collections",
-                new Date(),
-                false);
+    public List<Task> findAll(){
+        return service.findAll();
+    }
+
+    @PostMapping
+    public Task create(@Valid @RequestBody Task task){
+        return service.create(task);
     }
 }
