319a25cd4b8be9653032d979970976e23b193fca
Mateus Sandes <mateussandes@hotmail.com.br>
2024-11-11 20:09:13 -0300

    Adicionado a configuração do componente

--- basic-front/src/app/components/content/content.component.ts
+++ basic-front/src/app/components/content/content.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Task } from '../../models/task.model';
 import { TaskServiceService } from '../../services/task-service.service';
 import { CommonModule, NgFor } from '@angular/common';
+import { FormsModule, FormSubmittedEvent } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-content',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule
   ],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
@@ -16,16 +19,42 @@ export class ContentComponent implements OnInit {
 
   constructor(private taskService: TaskServiceService){}
 
-  public task!: Task;
+  public task: Task[] = [];
+  public newTask: Task = {
+    id: '',
+    taskName: '',
+    description: '',
+    createDate: new Date().toISOString(),
+    done: false,
+  }
 
   ngOnInit(): void {
     this.getTasks();
   }
 
   getTasks(): void {
-    this.taskService.getTask().subscribe((task: Task) => {
+    this.taskService.getTask().subscribe((task: Task[]) => {
       this.task = task;  // Aqui você define a variável task com os dados recebidos
       console.log("Exibindo os dados: ", this.task);
     });
+  }
+
+  createTask(): void {
+    this.taskService.createTask(this.newTask).subscribe(
+      (response: Task) => {
+        console.log("Tarefa criada com sucesso: ", response);
+        this.task.push(response);
+        this.newTask = {
+          id: '',
+          description: '',
+          taskName: '',
+          createDate: new Date().toISOString(),
+          done: false
+        };
+      },
+      (error) => {
+        console.error("Erro ao criar tarefa: ", error);
+      }
+    );
   }
 }
