68255b5656ce1716cefef5a92eabe33f9b72206f
Mateus Sandes <mateussandes@hotmail.com.br>
2024-11-11 20:05:51 -0300

    Adicionda a classe que contera a logica de programação do servidor

---
+++ toDoList/src/main/java/br/com/sandes/services/TaskService.java
@@ -0,0 +1,28 @@
+package br.com.sandes.services;
+
+import br.com.sandes.model.Task;
+import br.com.sandes.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository repository;
+
+    public List<Task> findAll(){
+        return repository.findAll();
+    }
+
+    public Task create(Task task){
+
+        if(task == null){
+            throw new IllegalArgumentException("Não é possivel persistir objetos nullos");
+        }
+
+        return repository.save(task);
+    }
+}
