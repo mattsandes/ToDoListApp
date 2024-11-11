b4be452b01d6a27bd0080778b1ab69bd20b454f6
Mateus Sandes <mateussandes@hotmail.com.br>
2024-11-11 20:01:45 -0300

    Adicionado a configuração para a comunicação com o backend

--- toDoList/src/main/java/br/com/sandes/config/WebConfig.java
+++ toDoList/src/main/java/br/com/sandes/config/WebConfig.java
@@ -13,11 +13,11 @@ public class WebConfig{
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Permite todas as rotas
-                        .allowedOrigins("http://localhost:4200") // Permite o frontend Angular
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true); // Se precisar enviar cookies ou autenticação
+                        .allowCredentials(true);
             }
         };
     }
