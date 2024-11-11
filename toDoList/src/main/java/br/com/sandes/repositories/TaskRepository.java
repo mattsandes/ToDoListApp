c4595f30d4251e13a085a41878d9d66708785758
Mateus Sandes <mateussandes@hotmail.com.br>
2024-11-11 20:04:53 -0300

    Adicionda a classe que fara a intermediação para o banco de dados

---
+++ toDoList/src/main/java/br/com/sandes/repositories/TaskRepository.java
@@ -0,0 +1,10 @@
+package br.com.sandes.repositories;
+
+import br.com.sandes.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {}
