8c392655a4322555f473c56f3c28e401a642b18b
Mateus Sandes <mateussandes@hotmail.com.br>
2024-11-15 20:54:03 -0300

    ADicionado as rotas para criar tasks e pesquisar tasks

--- basic-front/src/app/app.routes.ts
+++ basic-front/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { ContentComponent } from './components/content/content.component';
 import { PlaceholdersComponent } from './components/placeholders/placeholders.component';
 import { AboutComponent } from './components/about/about.component';
+import { CreateTaskComponent } from './components/create-task/create-task.component';
+import { SearchTasksComponent } from './components/search-tasks/search-tasks.component';
 
 export const routes: Routes = [
   {path: 'tasks', component: ContentComponent},
   {path: 'placeholder', component: PlaceholdersComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'createTask', component: CreateTaskComponent},
+  {path: 'searchTasks', component: SearchTasksComponent}
 ];
