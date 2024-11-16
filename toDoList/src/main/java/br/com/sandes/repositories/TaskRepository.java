938899cfded3581633364a890c667765f2fe9c67
Mateus Sandes <mateussandes@hotmail.com.br>
2024-11-15 21:03:02 -0300

    Adicionado a função findByTaskName e alterado a query para findAll

--- toDoList/src/main/java/br/com/sandes/repositories/TaskRepository.java
+++ toDoList/src/main/java/br/com/sandes/repositories/TaskRepository.java
@@ -2,9 +2,20 @@ package br.com.sandes.repositories;
 
 import br.com.sandes.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, UUID> {}
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+
+    @Query("SELECT task FROM Task task WHERE task.taskName LIKE '% %' AND task.taskName LIKE CONCAT('%', :taskName, '%')")
+    public Task findByTaskName(@Param("taskName") String taskName);
+
+    @Override
+    @Query("SELECT task FROM Task task ORDER BY task.createDate ASC")
+    public List<Task> findAll();
+}
