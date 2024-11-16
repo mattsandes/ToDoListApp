0fe88e4642d4deb7ed6fd8c8f6694e22b43ce976
Mateus Sandes <mateussandes@hotmail.com.br>
2024-11-16 19:24:58 -0300

    Refatorado a logica do componente para poder retornar pesquisas

--- basic-front/src/app/components/search-tasks/search-tasks.component.ts
+++ basic-front/src/app/components/search-tasks/search-tasks.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Task } from '../../models/task.model';
 import { TaskServiceService } from '../../services/task-service.service';
+import { setAlternateWeakRefImpl } from '@angular/core/primitives/signals';
 
 @Component({
   selector: 'app-search-tasks',
@@ -16,13 +17,9 @@ import { TaskServiceService } from '../../services/task-service.service';
 })
 export class SearchTasksComponent implements OnInit {
 
-  public task: Task = {
-    id: '',
-    taskName: '',
-    description: '',
-    createDate: '',
-    done: false
-  }
+  public taskQuery: string = '';
+  public task: Task | null = null;
+  public errorMessage: string = '';
 
   constructor(private service: TaskServiceService){}
 
@@ -30,24 +27,27 @@ export class SearchTasksComponent implements OnInit {
   }
 
   findTask(): void {
-    if(!this.task.taskName){
+    if(!this.taskQuery.trim()){
+      this.errorMessage = "Achei nada, oh XP"
       alert('Informe algo para fazer a pesquisa!');
+
+      return;
     }
 
-    this.service.findTask(this.task.taskName).subscribe({
-      next: (response: Task) => {
-        this.task = {
-          id: '',
-          taskName: '',
-          description: '',
-          createDate: new Date().toISOString(),
-          done: false
-        };
+    this.service.findTask(this.taskQuery).subscribe({
+      next: (response: Task | null) => {
+        if(response){
+          this.task = response;
 
-        return response;
+        } else {
+          alert("Não foi possivel encontrar essa tarefa!");
+          this.errorMessage = "Achei nada, oh XP"
+          this.task = null;
+        }
       },
       error: (error) => {
-        console.error('Não foi possivel encontrar a tarefa com esse nome');
+        alert('Não foi possivel encontrar a tarefa com esse nome');
+        this.task = null;
       }
     });
   }
