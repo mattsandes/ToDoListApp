e2580368fcc64159937ac956a1924b87969bb0cc
Mateus Sandes <mateussandes@hotmail.com.br>
2024-11-15 21:03:57 -0300

    Adicionado a função findbyTaskName

--- toDoList/src/main/java/br/com/sandes/services/TaskService.java
+++ toDoList/src/main/java/br/com/sandes/services/TaskService.java
@@ -3,6 +3,7 @@ package br.com.sandes.services;
 import br.com.sandes.model.Task;
 import br.com.sandes.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,5 +25,9 @@ public class TaskService {
         }
 
         return repository.save(task);
+    }
+
+    public Task findByTaskName(String taskName){
+        return repository.findByTaskName(taskName);
     }
 }
