cff1d8819ad032f6054c17c9878b213abf76fdf0
Mateus Sandes <mateussandes@hotmail.com.br>
2024-11-19 12:18:18 -0300

    Criada o model para a mensagem de erro

---
+++ toDoList/src/main/java/br/com/sandes/exceptions/ExceptionResponse.java
@@ -0,0 +1,45 @@
+package br.com.sandes.exceptions;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+public class ExceptionResponse implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Date timestamp;
+    private String message;
+    private String detail;
+
+    public ExceptionResponse(Date timestamp, String message, String detail) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.detail = detail;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
