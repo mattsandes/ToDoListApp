ee8e03e2388bbea49b660eb8235ad9f795bf3db8
Mateus Sandes <mateussandes@hotmail.com.br>
2024-11-19 12:18:46 -0300

    Criada o erro para quando o sistema não encontrar uma tarefa especifica

---
+++ toDoList/src/main/java/br/com/sandes/exceptions/TaskNotFoundException.java
@@ -0,0 +1,12 @@
+package br.com.sandes.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NO_CONTENT)
+public class TaskNotFoundException extends RuntimeException{
+
+    public TaskNotFoundException(String message){
+        super(message);
+    }
+}
