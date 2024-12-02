390d07b60bad268aee1e40d1d27e14977901c3b2
Mateus Sandes <mateussandes@hotmail.com.br>
2024-12-02 19:41:01 -0300

    Adiconado a dependencia do Routing Mdule

--- basic-front/src/app/components/header/header.component.ts
+++ basic-front/src/app/components/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu'
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     RouterLink,
+    RouterLinkActive,
     MatButtonModule,
     MatMenuModule
   ],
