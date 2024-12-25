8ba24c91f05bb03af0346bd66209266b094f6e24
Mateus Sandes <mateussandes@hotmail.com.br>
2024-12-25 16:11:00 -0300

    Adicionado o teste para markar uma tarefa como concluida

--- toDoList/src/test/java/br/com/sandes/service/TaskServiceTest.java
+++ toDoList/src/test/java/br/com/sandes/service/TaskServiceTest.java
@@ -15,8 +15,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -74,5 +73,31 @@ public class TaskServiceTest {
         assertEquals("Test Description", savedTask.getDescription());
         assertNotNull(savedTask.getCreateDate());
         assertEquals(false ,savedTask.getDone());
+    }
+
+    @DisplayName("Validar que é possivel marcar uma tarefa como completa")
+    @Test
+    void test_CanMarkTaskAsDone(){
+        //Given (Arrange);
+        Task task1 = new Task(
+                UUID.randomUUID(),
+                "New Task Name",
+                "Task description",
+                new Date(),
+                true);
+
+        given(service.doneTask(task1.getTaskName())).willReturn(task1);
+        //When (Act)
+
+        var result = service.doneTask(task1.getTaskName());
+
+        System.out.println(result.getDone());
+
+        //Then (Assert)
+        assertNotNull(result);
+        assertEquals("New Task Name", result.getTaskName());
+        assertEquals("Task description", result.getDescription());
+        assertNotNull(task1.getCreateDate());
+        assertEquals(true, result.getDone());
     }
 }
