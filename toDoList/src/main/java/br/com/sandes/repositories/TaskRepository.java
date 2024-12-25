ec6cb8b366780677416fe17c3f5951417b46a2d0
Mateus Sandes <mateussandes@hotmail.com.br>
2024-12-25 14:03:08 -0300

    Adicionado as dependencias para o repositorio

--- toDoList/src/main/java/br/com/sandes/repositories/TaskRepository.java
+++ toDoList/src/main/java/br/com/sandes/repositories/TaskRepository.java
@@ -1,7 +1,9 @@
 package br.com.sandes.repositories;
 
 import br.com.sandes.model.Task;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
