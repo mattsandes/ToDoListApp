b2145c7eeffefc09a35a31e19d9b142ae92dd87b
Mateus Sandes <mateussandes@hotmail.com.br>
2024-12-25 13:49:00 -0300

    Adicionado o metodo de finalizar tasks no content.component

--- basic-front/src/app/components/content/content.component.ts
+++ basic-front/src/app/components/content/content.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Task } from '../../models/task.model';
 import { TaskServiceService } from '../../services/task-service.service';
 import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table'
 import { MatProgressBarModule } from '@angular/material/progress-bar'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-content',
@@ -13,7 +16,8 @@ import { MatProgressBarModule } from '@angular/material/progress-bar'
     CommonModule,
     FormsModule,
     MatTableModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatCheckboxModule
   ],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
@@ -22,18 +26,35 @@ export class ContentComponent implements OnInit {
 
   constructor(private taskService: TaskServiceService){}
 
-  public task: Task[] = [];
-  public taskHeader: string[] = ['demo-taskName', 'demo-description', 'demo-createDate', 'demo-done'];
+  public tasks: Task[] = [];
+  public task?: Task
 
+  private _snackBar = inject(MatSnackBar);
+
+  public taskHeader: string[] = ['demo-taskName', 'demo-description', 'demo-createDate', 'demo-done', 'demo-select'];
 
   ngOnInit(): void {
     this.getTasks();
   }
 
   getTasks(): void {
-    this.taskService.getTask().subscribe((task: Task[]) => {
-      this.task = task;  // Aqui você define a variável task com os dados recebidos
-      return (this.task);
-    },
+    this.taskService.getTask().subscribe({
+      next: (tasks) => {
+        this.tasks = tasks;
+      },
+      error: (err) => {
+        console.log('Erro ao buscar tarefas: ', err)
+      }
+    }
   )};
+
+  markTaskAsDone(task: Task): void {
+    this.taskService.doneTask(task.taskName).subscribe({
+      next: (updatedTask) => {
+        location.reload();
+
+        console.log('Tarefa finalizada: ', task.taskName);
+      }
+    });
+  }
 }
